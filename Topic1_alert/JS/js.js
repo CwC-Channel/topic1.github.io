fd1242286558bf033fb19c985d7a8c3a2742768d
CwC-Channel <87448999+CwC-Channel@users.noreply.github.com>
2022-10-28 22:49:48 -0700

    ok

---
+++ Topic1_alert/JS/js.js
@@ -0,0 +1,14 @@
+
+// step 2: create functions 
+    function alert1() {
+      alert("this is Button1");
+    }
+    function alert2() {
+      alert("this is Button2");
+    }
+    function alert3() {
+      alert("this is Button3");
+    }
+
+
+    //step 3: add function to buttons
